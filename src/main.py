a550cb188f6564d3bcad1ad99ab6b80881d12816
keito0tada <keito0tada@gmail.com>
2023-10-12 14:26:03 +0900

    app_commandに変更

--- src/main.py
+++ src/main.py
@@ -7,35 +7,42 @@ from google.oauth2.service_account import Credentials
 from discord.ext import commands
 from .UtilityClasses_DiscordBot import base
 
+
 class ImageSender(base.Command):
     def __init__(self, bot: discord.ext.commands.Bot, allow_duplicated=False):
-        logging.info('idolquiz init')
+        logging.info("idolquiz init")
         super().__init__(bot, allow_duplicated)
-        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-        creds_env = os.getenv('GOOGLE_CREDENTIALS')
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/drive",
+        ]
+        creds_env = os.getenv("GOOGLE_CREDENTIALS")
         if creds_env is None:
             creds = Credentials.from_service_account_file(
-                "credentials.json",
-                scopes=scope
+                "credentials.json", scopes=scope
             )
         else:
             creds = Credentials.from_service_account_info(
-                json.loads(creds_env),
-                scopes=scope
+                json.loads(creds_env), scopes=scope
             )
         client = gspread.authorize(creds)
         url = "https://docs.google.com/spreadsheets/d/1-V7uZEYEXVG87a_OT9y5QE3PTltOZin0HnnY3iaDJJw/edit?usp=sharing"
         self.workbook = client.open_by_url(url)
 
-    @commands.command()
-    async def nogizaka(self, ctx: commands.Context):
+    @discord.app_commands.command(description="ランダムに取得します。")
+    async def random(self, interaction: discord.Interaction):
         member_list = self.workbook.worksheets()[0].get_all_values()
-        member_image_list = self.workbook.worksheets()[1].get_all_values() 
+        member_image_list = self.workbook.worksheets()[1].get_all_values()
         index = random.randint(0, len(member_image_list) - 1)
-        member = next(filter(lambda x: x[0] == member_image_list[index][0], member_list), '')
-        embed = discord.Embed(title=member_image_list[index][0], description='{}期'.format(member[1]))
+        member = next(
+            filter(lambda x: x[0] == member_image_list[index][0], member_list), ""
+        )
+        embed = discord.Embed(
+            title=member_image_list[index][0], description="{}期".format(member[1])
+        )
         embed.set_image(url=member_image_list[index][1])
-        await ctx.channel.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(ImageSender(bot=bot))
